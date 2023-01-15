6759aa4a72cb303a6fe36422a90ddba806203463
Taylor Van Anne <tjvananne@gmail.com>
2023-01-15 12:29:51 -0700

    initial setup of tests.

--- mytests.py
+++ mytests.py
@@ -1,26 +1,13 @@
 
-# todo, make this into real pytests.
-# I'm just experimenting for right now.
-
-# Test 1: debugging repo_path()
-from libwyag import repo_path, repo_find
-
-class DummyRepo(object):
-    def __init__(self, gitdir):
-        self.gitdir = gitdir
-
-myrepo = DummyRepo(gitdir = "project_dir")
-
-# both of these run successfully
-print(repo_path(myrepo, "some", "long", "path"))
-print(repo_path(myrepo, "one_str_path"))
+from libwyag import repo_find
 
 
 # Test 2: debugging repo_find
+# TODO: set up pytest-mock for this one
 import subprocess
-subprocess.run(["./wyag", "init", "/mnt/c/Users/Taylor/Documents/my_new_repo_from_wyag"])
+subprocess.run(["./wyag", "init", "../some_other_dir"])
 # set breakpoint here to inspect
-repo_find("/mnt/c/Users/Taylor/Documents/my_new_repo_from_wyag/folder1/folder2")
+# repo_find("/mnt/c/Users/Taylor/Documents/my_new_repo_from_wyag/folder1/folder2")
 
 
 print("done.")
